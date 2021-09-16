70f469719c68226f5f4be9c167552abb4b46b395
binkesi <sjtusgn@163.com>
2021-09-16 18:01:53 +0800

    Add server.

---
+++ day1-codec/server.go
@@ -0,0 +1,40 @@
+package day1codec
+
+import (
+	"log"
+	"net"
+	"wee_rpc/day1-codec/codec"
+)
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+func (s *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error: ", err)
+			return
+		}
+		go s.ServeConn(conn)
+	}
+}
+
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
