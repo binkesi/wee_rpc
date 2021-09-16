5e80d02dd171a0efff395f64a52f528baad285ca
binkesi <sjtusgn@163.com>
2021-09-16 17:25:46 +0800

    Add gobcodec encode/decode module.

--- day1-codec/codec/codec.go
+++ day1-codec/codec/codec.go
@@ -8,9 +8,25 @@ type Header struct {
 	Error         string
 }
 
-type codec interface {
+type Codec interface {
 	io.Closer
 	ReadHeader(*Header) error
 	ReadBody(interface{}) error
 	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
 }
