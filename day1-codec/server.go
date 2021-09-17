b49f6298b47565535d34c1cc3e2630c0af55250f
binkesi <sjtusgn@163.com>
2021-09-17 18:08:59 +0800

    Add read request for server.

--- day1-codec/server.go
+++ day1-codec/server.go
@@ -1,8 +1,12 @@
 package day1codec
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net"
+	"reflect"
+	"sync"
 	"wee_rpc/day1-codec/codec"
 )
 
@@ -11,6 +15,11 @@ const MagicNumber = 0x3bef5c
 type Option struct {
 	MagicNumber int
 	CodecType   codec.Type
+}
+
+type request struct {
+	h            *codec.Header
+	argv, replyv reflect.Value
 }
 
 var DefaultOption = &Option{
@@ -38,3 +47,68 @@ func (s *Server) Accept(lis net.Listener) {
 }
 
 func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+func (s *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server, decode option error: ", err)
+		return
+	}
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server, invalid magic number %x", opt.MagicNumber)
+		return
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server, invalid codec type %v", opt.CodecType)
+		return
+	}
+	s.ServeCodec(f(conn))
+}
+
+var invalidRequest = struct{}{}
+
+func (s *Server) ServeCodec(cc codec.Codec) {
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	for {
+		req, err := s.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			s.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go s.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+func (s *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error: ", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (s *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := s.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv error: ", err)
+	}
+	return req, nil
+}
