d40b13485d6f8f1e407eae4cce9410dd1ea6384b
binkesi <sjtusgn@163.com>
2021-09-22 21:04:03 +0800

    Start day2 client.

---
+++ day2-client/client.go
@@ -0,0 +1,14 @@
+package day2client
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
