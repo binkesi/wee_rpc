f2d549ffe64e0fa21bf8e8c435c07410c6cd68c4
binkesi <sjtusgn@163.com>
2021-09-26 16:02:58 +0800

    Add client close function.

--- day2-client/client.go
+++ day2-client/client.go
@@ -1,5 +1,12 @@
 package day2client
 
+import (
+	"errors"
+	"io"
+	"sync"
+	"wee_rpc/day1-codec/codec"
+)
+
 type Call struct {
 	Seq           uint64
 	ServiceMethod string
@@ -11,4 +18,36 @@ type Call struct {
 
 func (call *Call) done() {
 	call.Done <- call
+}
+
+type Client struct {
+	cc       codec.Codec
+	opt      *Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var _ io.Closer = (*Client)(nil)
+
+var ErrShutdown = errors.New("connection is shut down")
+
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing {
+		return ErrShutdown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
 }
