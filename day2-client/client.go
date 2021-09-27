662e3413f6f382f2c50c63489bc06492816cf182
binkesi <sjtusgn@163.com>
2021-09-27 15:56:21 +0800

    Add functions for client

--- day2-client/client.go
+++ day2-client/client.go
@@ -51,3 +51,35 @@ func (client *Client) IsAvailable() bool {
 	defer client.mu.Unlock()
 	return !client.shutdown && !client.closing
 }
+
+func (client *Client) registerCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+	call.Seq = client.seq
+	client.pending[call.Seq] = call
+	client.seq++
+	return call.Seq, nil
+}
+
+func (client *Client) removeCall(seq uint64) *Call {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	call := client.pending[seq]
+	delete(client.pending, seq)
+	return call
+}
+
+func (client *Client) terminateCalls(err error) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	client.shutdown = true
+	for _, call := range client.pending {
+		call.Error = err
+		call.done()
+	}
+}
