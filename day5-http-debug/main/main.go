65c0c140c95faec3a719ead3917afc2be474573c
binkesi <sjtusgn@163.com>
2021-10-13 11:33:04 +0800

    Finish http debug

--- day5-http-debug/main/main.go
+++ day5-http-debug/main/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 	day5httpdebug "wee_rpc/day5-http-debug"
@@ -24,26 +25,20 @@ func startServer(addr chan string) {
 	if err := day5httpdebug.Register(&foo); err != nil {
 		log.Fatal("register error:", err)
 	}
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", ":9999")
 	if err != nil {
 		log.Fatal("network error:", err)
 	}
 	log.Println("start rpc server on", l.Addr())
+	day5httpdebug.HandleHTTP()
 	addr <- l.Addr().String()
-	day5httpdebug.Accept(l)
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-
-	// in fact, following code is like a simple weerpc client
-	client, _ := day5httpdebug.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := day5httpdebug.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
-
 	time.Sleep(time.Second)
-	// send requests & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
@@ -58,4 +53,11 @@ func main() {
 		}(i)
 	}
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
